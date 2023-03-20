29d630271a77fb25e47276bac2d3ae9f1ae6a62f
NeelP2121 <neel.pullikol@gmail.com>
2023-03-20 15:00:50 -0400

    Square Root Function added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Main {
 
+	public double calcSqrt(double input){
+		return Math.sqrt(input);
+	}
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("-----------------------------------------");
