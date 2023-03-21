0bb176442f425ab1f05edaba0dbf3f233a4756a7
NeelP2121 <neel.pullikol@gmail.com>
2023-03-21 02:11:51 -0400

    Added new tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,47 +32,48 @@ public class Main {
 	public static void main(String[] args){
 		
 		Main obj = new Main();
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("-----------------------------------------");
-		System.out.println("--------------------MENU-----------------");
-		System.out.println("-  Select one of the following options :-");
-		System.out.println("-1.) Square Root.                       -");
-		System.out.println("-2.) Factorial.                         -");
-		System.out.println("-3.) Natural Logarithm.                 -");
-		System.out.println("-4.) Power.                             -");
-		System.out.println("-----------------------------------------");
+		while(true){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("-----------------------------------------");
+			System.out.println("--------------------MENU-----------------");
+			System.out.println("-  Select one of the following options :-");
+			System.out.println("-1.) Square Root.                       -");
+			System.out.println("-2.) Factorial.                         -");
+			System.out.println("-3.) Natural Logarithm.                 -");
+			System.out.println("-4.) Power.                             -");
+			System.out.println("-----------------------------------------");
 
-		int choice = sc.nextInt();
+			int choice = sc.nextInt();
 
-		System.out.println();
+			System.out.println();
 
-		switch(choice){
-			case 1:
-				System.out.println("Enter number to calculate Square Root");
-				double inputS = sc.nextDouble();
-				System.out.println(obj.calcSqrt(inputS));
-				break;
-			case 2:
-				System.out.println("Enter number to calculate Factorial");
-				int inputF = sc.nextInt();
-				System.out.println(obj.calcFactorial(inputF));
-				break;
-			case 3:
-				System.out.println("Enter number to calculate Natural Logarithm");
-				double inputL = sc.nextDouble();
-				System.out.println(obj.calcLogarithm(inputL));
-				break;
-			case 4:
-				System.out.println("Enter base number in Power");
-				int inputBase = sc.nextInt();
-				System.out.println("\nEnter exponent in Power");
-				int inputExp = sc.nextInt();
-				System.out.println(obj.calcPower(inputBase, inputExp));
-				break;
-			default:
-				System.out.println("-----------------------------------------");
-				System.out.println("---Chosen option not existent in menu----");
+			switch(choice){
+				case 1:
+					System.out.println("Enter number to calculate Square Root");
+					double inputS = sc.nextDouble();
+					System.out.println(obj.calcSqrt(inputS));
+					break;
+				case 2:
+					System.out.println("Enter number to calculate Factorial");
+					int inputF = sc.nextInt();
+					System.out.println(obj.calcFactorial(inputF));
+					break;
+				case 3:
+					System.out.println("Enter number to calculate Natural Logarithm");
+					double inputL = sc.nextDouble();
+					System.out.println(obj.calcLogarithm(inputL));
+					break;
+				case 4:
+					System.out.println("Enter base number in Power");
+					int inputBase = sc.nextInt();
+					System.out.println("\nEnter exponent in Power");
+					int inputExp = sc.nextInt();
+					System.out.println(obj.calcPower(inputBase, inputExp));
+					break;
+				default:
+					System.out.println("-----------------------------------------");
+					System.out.println("---Chosen option not existent in menu----");
+			}
 		}
 			
 	}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 
 public class MainTest {
 
+	Main obj = new Main();
     @Test
     public void testFindMax(){
+	assertEquals("Testing Square Root ", 2.0, obj.calcSqrt(4.0), 0.001);
+	assertEquals("Testing Factorual ", 120, obj.calcFactorial(5));
+	assertEquals("Testing Natural Logarithm ", 2.30258509299, obj.calcLogarithm(10.0), 0.001);
+	assertEquals("Testing Power function ", 8.0, obj.calcPower(2, 3), 0.001);
     }
 }  
